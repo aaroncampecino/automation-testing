10cbd618173b365ec30befc7e536c1480d42e2b3
Aaron Campecino <aaroncampecino@gmail.com>
2022-02-20 14:30:39 +0800

    updates

---
+++ src/main/java/com/automation/testing/core/TestArtifacts.java
@@ -0,0 +1,66 @@
+package com.automation.testing.core;
+
+import com.automation.testing.model.KeyValuePair;
+import com.automation.testing.utility.CsvUtility;
+import com.automation.testing.utility.TableGenerator;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class TestArtifacts {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestArtifacts.class);
+
+    private static TestArtifacts instance;
+    private Map<String, String> artifacts = new HashMap<>();
+
+    private TestArtifacts(){}
+
+    public static TestArtifacts getInstance(){
+        if(instance == null)
+            instance = new TestArtifacts();
+
+        return instance;
+    }
+
+    public Map<String, String> getArtifacts(){
+        return artifacts;
+    }
+
+    public void putArtifacts(String key, String value){
+        if(StringUtils.isBlank(key))
+            return;
+        artifacts.put(key, value);
+    }
+
+    public void addKeyValuePair(List<KeyValuePair> keyValuePairList){
+        keyValuePairList.forEach(e->{
+            putArtifacts(e.getKey(),e.getValue());
+        });
+    }
+
+    public void printTestArtifacts(){
+        if(artifacts.isEmpty()) {
+            LOGGER.info("No test artifacts recovered.");
+            return;
+        }
+
+        List<String> headers = Arrays.asList("Key","Value");
+
+        List<List<String>> data = new ArrayList<>();
+        List<String> row;
+
+        for (var entry : artifacts.entrySet()) {
+            row = new ArrayList<>();
+            row.add(entry.getKey());
+            row.add(entry.getValue());
+            data.add(row);
+        }
+
+        TableGenerator.generateTable(headers, data);
+
+    }
+
+}
--- src/main/java/com/automation/testing/utility/CsvUtility.java
+++ src/main/java/com/automation/testing/utility/CsvUtility.java
@@ -1,48 +1,126 @@
 package com.automation.testing.utility;
 
-import com.automation.testing.model.Fixture;
+import com.automation.testing.core.TestArtifacts;
+import com.automation.testing.model.KeyValuePair;
+import com.automation.testing.model.TestSteps;
+import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.MappingStrategy;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CsvUtility {
 
-    private static List<Fixture> csvLists;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CsvUtility.class);
 
-    public static List<Fixture> beanBuilderExample(Path path, Class clazz) throws IOException {
-        return beanBuilderExample(path, clazz, new ColumnPositionMappingStrategy());
+    /**
+     * @param path path where csv is located
+     * @param clazz the class where key-value property is located
+     * @return a list of key-value pairs
+     * */
+    public static List<KeyValuePair> buildKeyValueMapping(String path, Class clazz) throws IOException {
+        return buildKeyValueMapping(path, clazz, new ColumnPositionMappingStrategy());
     }
 
-    public static List<Fixture> beanBuilderExample(Path path, Class clazz, MappingStrategy ms) throws IOException {
+    /**
+     * @param path path where csv is located
+     * @param clazz the class where key-value property is located
+     * @param mappingStrategy example ColumnPositionMappingStrategy
+     * @return a list of key-value pairs
+     * */
+    public static List<KeyValuePair> buildKeyValueMapping(String strPath, Class clazz, MappingStrategy mappingStrategy) throws IOException {
 
-        ms.setType(clazz);
+        mappingStrategy.setType(clazz);
+
+        var file = new File(strPath).getAbsolutePath();
+
+        Path path = Paths.get(file);
 
         Reader reader = Files.newBufferedReader(path);
         CsvToBean cb = new CsvToBeanBuilder(reader).withType(clazz)
-                .withMappingStrategy(ms)
+                .withMappingStrategy(mappingStrategy)
                 .build();
 
-        setCsvList(cb.parse());
+        List<KeyValuePair> pairs = cb.parse();
+
         reader.close();
 
-        return getCsvList();
+        return pairs;
     }
 
-    private static void setCsvList(List<Fixture> csvList) {
-        csvLists = csvList;
+    /**
+     * To get the list of test steps
+     * @param path the path where test steps located
+     * @return the list of TestSteps object
+     * */
+
+    public static List<TestSteps> buildTestSteps(Path path) throws IOException {
+        return buildTestSteps(path, "default");
     }
 
-    public static List<Fixture> getCsvList() {
-        if (csvLists != null) return csvLists;
-        return new ArrayList<>();
+    /**
+     * To get the list of test steps
+     * @param path the path where test steps located
+     * @param environment specifies the environment to use
+     * @return the list of TestSteps object
+     * */
+    public static List<TestSteps> buildTestSteps(Path path, String environment) throws IOException {
+        List<TestSteps> stepsList = new ArrayList<>();
+
+        if(StringUtils.isBlank(environment))
+            environment = "default";
+
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVReader csvReader = new CSVReader(reader);
+        String[] line;
+        int valueIndex = -1;
+        boolean bolIsHeader = true;
+        while ((line = csvReader.readNext()) != null) {
+            if(bolIsHeader){
+                valueIndex = getValueIndex(line, environment);
+                if(valueIndex == -1)
+                    throw new IndexOutOfBoundsException("No environment value specified.");
+
+                bolIsHeader = false;
+                continue;
+            }
+            stepsList.add(getTestSteps(line, valueIndex));
+        }
+        reader.close();
+        csvReader.close();
+
+        return stepsList;
     }
 
+    private static TestSteps getTestSteps(String[] line, int valueIndex){
+        TestSteps testSteps = new TestSteps();
+        testSteps.setStep(line[0]);
+        testSteps.setScreenName(line[1]);
+        testSteps.setElementName(line[2]);
+        testSteps.setValue(line[valueIndex]);
+        return testSteps;
+    }
+
+    private static int getValueIndex(String[] line, String environment){
+        int index = -1;
+        for (String s : line) {
+            ++index;
+            if(StringUtils.equalsIgnoreCase(s.trim(),environment+"_value"))
+                return index;
+        }
+        return -1;
+    }
 }
